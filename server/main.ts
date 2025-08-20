9019ad9c9b2314517b5fda45fcd6600658654cef
Oumate <alhadjioum0@gmail.com>
2025-08-20 10:09:25 +0100

    feat: setting up mongoose connection to the db

--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,16 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import { error } from "console";
+
+
+await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/blog-posts")
+.then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((error) => {
+  console.log("Error connecting to MongoDB:", error);
+})
 
 const app = express();
 
@@ -9,6 +20,6 @@ app.get('/', (req, res) => {
   res.json({ message: "Welcome to the Blog Post app Server" })
 })
 
-app.listen(8000, () => {
-  console.log("Server is running on: http://localhost:8000");
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is running on: http://localhost:${process.env.PORT || 8000}`);
 });
