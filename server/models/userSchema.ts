1d005bfeb5146450e786b582a73f1a67c22849c4
Oumate <alhadjioum0@gmail.com>
2025-08-27 07:48:38 +0100

    feat: added user defined roles

--- server/models/userSchema.ts
+++ server/models/userSchema.ts
@@ -1,11 +1,19 @@
 // User Schema definition
 
 import mongoose from 'mongoose';
+import { email, lowercase, object, string } from 'zod';
+
+// enum of roles
+enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
 
 interface IUser extends mongoose.Document {
   username: string;
   email: string;
   password: string;
+  role: UserRole;
   posts: mongoose.Types.ObjectId[];
   createdAt: Date;
   updatedAt: Date;
@@ -16,17 +24,31 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    lowercase: true,
     unique: true,
+    trim: true,
     validate: {
-      validator: (email: string) => email.includes('@'),
+      validator: (email: string) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      },
       message: 'Invalid email format',
     },
   },
   password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: Object.values(UserRole), // ['admin', 'user']
+    default: UserRole.USER,
+    required: true,
+  },
   posts: [{ type: mongoose.Types.ObjectId, ref: 'BlogPost' }],
   createdAt: { type: Date, required: true, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
+
+UserSchema.index({ username: 1 });
+UserSchema.index({ role: 1 });
 
 // Middleware: When a user is deleted, delete all their blog posts
 UserSchema.post('findOneAndDelete', async function (doc) {
@@ -52,6 +74,5 @@ UserSchema.post('deleteMany', async function () {
   }
 });
 
-const User = mongoose.model<IUser>('User', UserSchema);
-
-export default User;
+export { UserRole };
+export default mongoose.model<IUser>('User', UserSchema);
