de1ccd57455df5b33402a51f5ef91d5aee8c69d4
Oumate <alhadjioum0@gmail.com>
2025-09-04 12:48:40 +0100

    feat: implement theme-based styling in App component for dark mode and light mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import './App.css';
 import Footer from './components/Footer.tsx';
 import NavBar from './components/NavBar.tsx';
 import Home from './pages/Home.tsx';
+import useThemeStore from './stores/themeStore.ts';
 
 function App() {
+  const { isDarkMode } = useThemeStore();
   return (
-    <div className='bg-gray-100'>
+    <div className={isDarkMode ? 'bg-[#111e2a] text-gray-600' : 'bg-gray-200'}>
       <header>
         <NavBar />
       </header>
