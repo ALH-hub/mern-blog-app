c7cfb91cdffff1e9e3bb242b660ec7ee60bc3305
Oumate <alhadjioum0@gmail.com>
2025-09-04 12:42:15 +0100

    feat: implement dynamic theme support in Footer component for dark mode and light mode styling

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,14 @@
+import useThemeStore from '../stores/themeStore';
+
 const Footer = () => {
+  const { isDarkMode } = useThemeStore();
   return (
-    <div className='bg-gray-800 text-white py-4'>
+    <div
+      className={`bg-gray-800 py-4 ${
+        isDarkMode ? 'bg-gray-800 text-white' : 'bg-white text-gray-600'
+      }`}
+    >
+      <div></div>
       <p className='text-center'>© 2023 NexusBlog. All rights reserved.</p>
     </div>
   );
