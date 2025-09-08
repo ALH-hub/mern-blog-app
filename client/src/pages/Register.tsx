9fb24221c608c125ea77b9b89f3b0ca87687af1a
Oumate <alhadjioum0@gmail.com>
2025-09-08 17:02:19 +0100

    feat: update Register component layout for improved structure and clarity

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -4,10 +4,12 @@ import Button from '../common/Button';
 const Register = () => {
   return (
     <div className='  w-full max-h-screen px-8 pb-8 rounded-lg shadow-lg gap-16 bg-white'>
-      <Link to='/' className='flex justify-between items-center w-full'>
-        <img className='w-70 h-42' src='/logo.png' alt='Logo image' />
-        <h1 className='text-3xl font-bold text-[#1d4ed8]'>Login</h1>
-      </Link>
+      <div className='flex justify-between items-center w-full'>
+        <Link to={'/'}>
+          <img className='w-70 h-42' src='/logo.png' alt='Logo image' />
+        </Link>
+        <h1 className='text-3xl font-bold text-[#1d4ed8]'>Registration</h1>
+      </div>
       <div className='flex flex-col items-center justify-center  gap-8'>
         <form className='w-full flex flex-col gap-6'>
           <input
