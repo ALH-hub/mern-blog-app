8ca44318e9350496f246febdec8dae34349b1bf6
Oumate <alhadjioum0@gmail.com>
2025-09-08 15:19:37 +0100

    feat: update Footer, NavBar, and Login components for improved layout and navigation

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,11 +4,11 @@ const Footer = () => {
   const { isDarkMode } = useThemeStore();
   return (
     <div
-      className={`bg-gray-800 w-full flex flex-col justify-center items-center py-8 ${
+      className={`bg-gray-800 w-full flex flex-col justify-center items-center py-8 mt-16 ${
         isDarkMode ? 'bg-white text-gray-800 ' : 'bg-gray-800 text-white '
       }`}
     >
-      <div className='w-full max-w-6xl flex justify-center items-center p-4 gap-4'>
+      {/* <div className='w-full max-w-6xl flex justify-center items-center p-4 gap-4'>
         <div>
           <img src='icon.png' alt='image here' className='h-20 w-20' />
           <p>
@@ -28,7 +28,7 @@ const Footer = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
           necessitatibus odit, sint possimus blanditiis at.
         </div>
-      </div>
+      </div> */}
       <p className='text-center'>© 2023 NexusBlog. All rights reserved.</p>
     </div>
   );
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router';
 import Button from '../common/Button';
 import useThemeStore from '../stores/themeStore';
+import { useNavigate } from 'react-router';
 
 function NavBar() {
   const { isDarkMode, toggleDarkMode } = useThemeStore();
+  const navigate = useNavigate();
 
   return (
     <div
@@ -10,20 +13,20 @@ function NavBar() {
         isDarkMode ? 'bg-gray-800' : 'bg-white'
       }`}
     >
-      <div className='flex items-center'>
+      <Link to='/' className='flex items-center'>
         <img src='icon.png' alt='NexusBlog Icon' className='h-12' />
         <h1 className={`text-3xl font-bold text-[#544cdb]`}>NexusBlog</h1>
-      </div>
+      </Link>
       <div
         className={`flex gap-6 text-lg font-medium ${
           isDarkMode ? 'text-gray-300' : 'text-gray-700'
         }`}
       >
-        <button>Home</button>
-        <button>Discover</button>
-        <button>Categories</button>
-        <button>About</button>
-        <button>Contact</button>
+        <Link to='/'>Home</Link>
+        <Link to='/discover'>Discover</Link>
+        <Link to='/categories'>Categories</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
       </div>
       <div className='flex items-center gap-3'>
         <button
@@ -38,14 +41,11 @@ function NavBar() {
             }`}
           ></i>
         </button>
-        <Button onClick={() => console.log('Login clicked')} variant='outline'>
+        <Button onClick={() => navigate('/auth/login')} variant='outline'>
           <i className='fas fa-right-to-bracket'></i>
           <span>Login</span>
         </Button>
-        <Button
-          onClick={() => console.log('Sign Up clicked')}
-          variant='primary'
-        >
+        <Button onClick={() => navigate('/auth/register')} variant='primary'>
           <i className='fas fa-user-plus'></i>
           <span>Sign Up</span>
         </Button>
