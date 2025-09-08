f4b2d665f5cd49fac459aeb35a1991bc4dc08993
Oumate <alhadjioum0@gmail.com>
2025-09-08 13:03:44 +0100

    feat: refactor App component to implement routing and layout structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Footer from './components/Footer.tsx';
 import NavBar from './components/NavBar.tsx';
+import About from './pages/About.tsx';
+import Categories from './pages/Categories.tsx';
+import Contact from './pages/Contact.tsx';
+import Discover from './pages/Discover.tsx';
 import Home from './pages/Home.tsx';
 import useThemeStore from './stores/themeStore.ts';
+import { Outlet, createBrowserRouter, RouterProvider } from 'react-router';
 
-function App() {
+const Layout = () => {
   const { isDarkMode } = useThemeStore();
   return (
     <div className={isDarkMode ? 'bg-[#111e2a] text-white' : 'bg-gray-200'}>
@@ -12,11 +17,52 @@ function App() {
         <NavBar />
       </header>
       <main>
-        <Home />
+        <Outlet />
       </main>
       <footer>
         <Footer />
       </footer>
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/categories',
+        element: <Categories />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/discover',
+        element: <Discover />,
+      },
+      // {
+      //   path: '/404',
+      //   element: <NotFound />,
+      // },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
