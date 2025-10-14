c016a7efc2f341de14676f6a1623cf7a47dc9005
Alhadji Oumate <alhadjioum0@gmail.com>
2025-10-14 15:32:27 +0100

    fix: update Home component to apply dark mode styles and remove unused mock posts

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,11 +5,13 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import type { Post } from '../types';
 import postService from '../services/postService';
+import useThemeStore from '../stores/themeStore';
 
 function Home() {
   const { isAuthenticated } = useAuthStore();
   const [featuredPosts, setFeaturedPosts] = useState<Post[]>([]);
   const [isLoading, setisLoading] = useState(false);
+  const { isDarkMode } = useThemeStore();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -28,40 +30,17 @@ function Home() {
       } finally {
         setisLoading(false);
       }
-
-      // const mockPosts: Post[] = [
-      //   {
-      //     _id: '1',
-      //     title: 'The Future of Technology',
-      //     content: 'Full content of the post...',
-      //     excerpt: 'A brief overview of upcoming tech trends.',
-      //     category: 'Technology',
-      //     author: { _id: 'a1', username: 'tech_guru' },
-      //     coverImage:
-      //       'https://media.istockphoto.com/id/1316372349/photo/shot-of-a-team-of-young-businesspeople-using-a-laptop-during-a-late-night-meeting-in-a-modern.jpg?s=1024x1024&w=is&k=20&c=vztaIB-e8bsHk6DFUpOWC_IykTIxqzqV77ZokInklGk=',
-      //     createdAt: new Date().toISOString(),
-      //     readingTime: 5,
-      //   },
-      //   {
-      //     _id: '2',
-      //     title: 'Healthy Living Tips',
-      //     content: 'Full content of the post...',
-      //     excerpt: 'Simple tips for a healthier lifestyle.',
-      //     category: 'Health and Wellness',
-      //     author: { _id: 'a2', username: 'health_expert' },
-      //     coverImage:
-      //       'https://media.istockphoto.com/id/1316372349/photo/shot-of-a-team-of-young-businesspeople-using-a-laptop-during-a-late-night-meeting-in-a-modern.jpg?s=1024x1024&w=is&k=20&c=vztaIB-e8bsHk6DFUpOWC_IykTIxqzqV77ZokInklGk=',
-      //     createdAt: new Date().toISOString(),
-      //     readingTime: 4,
-      //   },
-      // ];
     };
 
     fetchFeaturedPosts();
   }, [isAuthenticated, navigate]);
 
   return (
-    <div className='flex flex-col items-center justify-center pt-32'>
+    <div
+      className={`flex flex-col items-center justify-center pt-32 ${
+        isDarkMode ? `text-white` : ''
+      }`}
+    >
       <div className='p-6 flex items-center justify-center flex-col gap-10 w-full'>
         <h1 className='text-[3.5rem] leading-none font-bold text-center tracking-tighter flex flex-col items-center justify-center bg-gradient-to-r from-[#544cdb] via-purple-600 to-pink-600 bg-clip-text text-transparent animate-gradient bg-[length:200%_200%]'>
           Where Ideas Connect and <br /> Stories Unfold.
@@ -99,14 +78,14 @@ function Home() {
           Featured Articles
         </h2>
         <hr className='w-16 border-2 border-[#544cdb]' />
-        <div className=' w-full grid grid-cols-1 md:grid-cols-3 gap-8 mt-20'>
+        <div className={`w-full grid grid-cols-1 md:grid-cols-3 gap-8 mt-20`}>
           {isLoading ? (
             <p className='text-gray-600'>Loading...</p>
           ) : (
             featuredPosts.map((post) => (
               <article
                 key={post._id}
-                className='hover:shadow-lg rounded bg-white overflow-hidden group cursor-pointer flex flex-col'
+                className={`hover:shadow-lg rounded bg-white overflow-hidden group cursor-pointer flex flex-col text-black`}
                 onClick={() => navigate(`/post/${post._id}`)}
               >
                 {/* Post Image */}
@@ -166,12 +145,12 @@ function Home() {
           )}
         </div>
       </div>
-      <div className=' mt-10 px-2 py-8 flex justify-center flex-col align-items  w-full max-w-6xl'>
+      <div className='mt-10 px-2 py-8 flex justify-center flex-col align-items  w-full max-w-6xl'>
         <h3 className='text-4xl font-semibold text-left tracking-tighter'>
           Explore Categories
         </h3>
         <hr className='w-16 border-2 border-[#544cdb]' />
-        <div className='w-full grid grid-cols-2 md:grid-cols-4 gap-8 mt-20'>
+        <div className='w-full grid grid-cols-2 md:grid-cols-4 gap-8 mt-20 text-black'>
           <div className='bg-white p-4 rounded-lg flex flex-col items-center gap-4 hover:shadow-lg cursor-pointer'>
             <i className='fas fa-laptop-code text-5xl text-[#544cdb] '></i>
             <h3 className='text-xl font-semibold'>Technology</h3>
